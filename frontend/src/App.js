624e173f5cca28a41f26fdbfb1bf41c6b99bb9d2
richie48 <odiorrichard@gmail.com>
2021-04-01 00:32:09 +0100

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './HomeScreen';
 
 const App = () => {
   return (
@@ -9,7 +10,9 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>welcome to my store</h1>
+          <h1>
+            <HomeScreen />
+          </h1>
         </Container>
       </main>
       <Footer />
