f0a4cb7c0d46e742927eb76b79939d0a0059feb9
richie48 <odiorrichard@gmail.com>
2021-04-03 00:49:03 +0100

    fifth commit

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 // import products from '../src/products';
 import Product from './components/Product';
+import axios from 'axios';
 
 const HomeScreen = () => {
-  const [products, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  //i can't make the arrow function in my use-effect async,need a seperate function
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get('/api/products');
+      //we can get access to res.data
+      setProducts(res.data);
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <div>
--- frontend/src/ProductScreen.js
+++ frontend/src/ProductScreen.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from './Rating';
-import products from './products';
+import axios from 'axios';
+// import products from './products';
 
 //Have to bring in match as props efore using match.params.id
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const res = await axios.get(`/api/products/${match.params.id}`);
+      //we can get access to res.data
+      setProduct(res.data);
+    };
+    fetchProduct();
+  }, []);
+
   return (
     <div>
       <Link className="btn btn-light my-3" to="/">
