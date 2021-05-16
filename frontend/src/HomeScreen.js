3d89a7daa76089531ded23df78bbdf60004cc5d0
richie48 <odiorrichard@gmail.com>
2021-05-16 23:52:50 +0100

    changes

--- frontend/src/HomeScreen.js
+++ frontend/src/HomeScreen.js
@@ -2,20 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 // import products from '../src/products';
 import Product from './components/Product';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../src/actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  //to be able to display our states in redux we use selector,it takes in the state.
+  const productList = useSelector((state) => {
+    state.productList;
+  });
+  //we can pull out all we want from the state
+  const { loading, error, products } = productList;
 
   //i can't make the arrow function in my use-effect async,need a seperate function
   useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get('/api/products');
-      //we can get access to res.data
-      setProducts(res.data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]); //passed it in cause its a dependency
 
   return (
     <div>
